eb30d2646e3053c6bb50412def343c80886a2780
Yegon Kipkirui Geoffrey <52694586+ipkiruiYegon@users.noreply.github.com>
2020-09-04 16:23:41 +0300

    Initial set up for DigiVet App Hello world

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['@react-native-community'],
+  rules: {
+    'comma-dangle': ['error', 'never'],
+    'react/jsx-props-no-spreading': 0,
+    'react/jsx-curly-newline': 0
+  }
+};
---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  bracketSpacing: false,
+  jsxBracketSameLine: true,
+  trailingComma: 'none',
+  semi: true,
+  singleQuote: true,
+  endOfLine: 'crlf'
+};
